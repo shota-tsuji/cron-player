af69fdb1312c10326a83787cf0096e1ad058e664
shota-tsuji <shotatuzi@gmail.com>
2022-11-20 07:30:42 +0900

    feat: fuction to play sound is created

--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,28 @@ use std::io::BufReader;
 use chrono::Utc;
 use tokio_cron_scheduler::{Job, JobScheduler};
 
+fn play_sound(file: &String) {
+    let (_stream, handle) = rodio::OutputStream::try_default().unwrap();
+    let sink = rodio::Sink::try_new(&handle).unwrap();
+
+    let file = std::fs::File::open(file).unwrap();
+    sink.append(rodio::Decoder::new(BufReader::new(file)).unwrap());
+
+    sink.sleep_until_end();
+}
+
 #[tokio::main]
 async fn main() {
     let args: Vec<String> = env::args().collect();
     println!("{:?}", args);
 
 
-    let expression = "0 1/2 * * * * *";
+    let expression = "0 1/1 * * * * *";
     let mut sched = JobScheduler::new().await.unwrap();
 
     let job = Job::new(expression, move |_uuid, _l| {
         println!("{:?}", Utc::now());
-        let (_stream, handle) = rodio::OutputStream::try_default().unwrap();
-        let sink = rodio::Sink::try_new(&handle).unwrap();
-
-        let file = std::fs::File::open(&args[1]).unwrap();
-        sink.append(rodio::Decoder::new(BufReader::new(file)).unwrap());
-
-        sink.sleep_until_end();
+        play_sound(&args[1]);
         println!("{:?}", Utc::now());
     }).unwrap();
 
