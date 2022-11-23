f64b6ab824283b2c6ece15b7762a8fcd883fea9e
shota-tsuji <shotatuzi@gmail.com>
2022-11-23 11:39:52 +0900

    refactor: error handling is added

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
-use std::env;
+use std::{env, fmt};
+use std::fmt::Formatter;
 use std::io::BufReader;
 use chrono::Utc;
 use tokio_cron_scheduler::{Job, JobScheduler};
 
-fn play_sound(file: &std::fs::File) {
-    let file = file.try_clone().unwrap();
+fn play_sound(file: &std::fs::File) -> Result<(), ProcessError>{
+    let file = file.try_clone().map_err(|_| ProcessError::FileReadError)?;
+    let decoder = rodio::Decoder::new(BufReader::new(file)).map_err(|_| ProcessError::FileReadError)?;
+    // OutputStream (_stream) is needed to play sound.
+    let (_stream, handle) = rodio::OutputStream::try_default().map_err(|_| ProcessError::OutputStreamError)?;
+    let sink = rodio::Sink::try_new(&handle).map_err(|_| ProcessError::OutputStreamError)?;
 
-    let decoder = rodio::Decoder::new(BufReader::new(file)).unwrap();
-    let (_stream, handle) = rodio::OutputStream::try_default().unwrap();
-    let sink = rodio::Sink::try_new(&handle).unwrap();
     sink.append(decoder);
     sink.sleep_until_end();
+    Ok(())
 }
 
 #[tokio::main]
@@ -22,7 +25,10 @@ async fn main() {
         println!("{:?}", Utc::now());
         let filename = env::args().nth(1).unwrap();
         let file = std::fs::File::open(filename).unwrap();
-        play_sound(&file);
+        match play_sound(&file) {
+            Err(err) => println!("{}", err),
+            _ => {},
+        }
         println!("{:?}", Utc::now());
     }).unwrap();
 
@@ -42,4 +48,18 @@ async fn main() {
         tokio::time::sleep(core::time::Duration::from_millis(500)).await;
     }
 
+}
+
+enum ProcessError {
+    FileReadError,
+    OutputStreamError,
+}
+
+impl fmt::Display for ProcessError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match *self {
+            ProcessError::FileReadError => write!(f, "file state is invalid"),
+            ProcessError::OutputStreamError => write!(f, "output stream setting is failed"),
+        }
+    }
 }
